37acabef0db98c1a07326ea022bfc5ef1d328d8c
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2022-09-01 01:10:05 +0530

    Time: 144 ms (78.21%), Space: 14.9 MB (68.27%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int rowindex = 0;
+        int colindex = col-1;
+        while(rowindex<row && colindex>=0){
+            int element = matrix[rowindex][colindex];
+            if(element==target){
+                return 1;
+            }
+            else if(element<target){
+                rowindex++;
+            }
+            else if(element>target){
+                colindex--;
+            }
+        }
+        return 0;
+    }
+};
