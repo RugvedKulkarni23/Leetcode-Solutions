78733252809fb41f25c1c2c90bc2d2691ee2f9ed
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2022-09-25 18:57:53 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (81.79%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(int i=0;s[i]!='\0';i++)
-	{
-		if (s[i] >= 'A' && s[i] <= 'Z')    //checking for uppercase characters
-			s[i] = s[i] + 32;              //converting uppercase to lowercase
-	}
+        transform(s.begin(),s.end(),s.begin(),::tolower);
         return s;
     }
 };
