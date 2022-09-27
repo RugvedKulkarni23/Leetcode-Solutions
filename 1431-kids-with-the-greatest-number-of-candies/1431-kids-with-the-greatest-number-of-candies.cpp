108809242a523b74caccb493f5bc3d833ea54ac0
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2022-09-27 23:20:47 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (59.63%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max=0;
+        vector<bool> ans;
+        for(int i=0;i<candies.size();i++){
+            if(max<candies[i]){
+                max= candies[i];
+            }
+            
+        }
+        for(int i=0;i<candies.size();i++){
+            if((candies[i] + extraCandies)>= max){
+                ans.push_back(true);
+                
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+        return ans;
+        
+    }
+};
