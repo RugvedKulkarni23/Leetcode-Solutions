cb346ed5bc6af9b80415eee19938f65ff5b73bb1
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2023-03-10 02:02:19 +0530

    Time: 12 ms (43.99%), Space: 7.7 MB (68.68%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL|| head->next==NULL){
+         if(head==NULL|| head->next==NULL){
              return NULL;
         }
         ListNode*slow = head;
