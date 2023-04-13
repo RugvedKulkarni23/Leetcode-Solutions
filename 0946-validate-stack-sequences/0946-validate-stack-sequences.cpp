a55604eab3c502b7322f2ec77f134d6329cc8e3c
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2023-04-13 23:57:51 +0530

    Time: 7 ms (85.57%), Space: 15.2 MB (98.01%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int i = 0;
+        for (int num : pushed) {
+            st.push(num);
+            while (!st.empty() && st.top() == popped[i]) {
+                st.pop();
+                i++;
+            }
+        }
+        return st.empty();
+    }
+};
