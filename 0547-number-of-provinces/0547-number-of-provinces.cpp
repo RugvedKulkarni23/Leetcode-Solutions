87e6c83547e2fbac16228fc08c0a50fa30bb74c3
RugvedKulkarni23 <92722953+RugvedKulkarni23@users.noreply.github.com>
2023-06-04 22:34:43 +0530

    Time: 32 ms (27.60%), Space: 14.5 MB (48.35%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int visited[205];
+    void check(vector<int> v[],int itr)
+    {
+        if(visited[itr]) 
+            return;
+
+        visited[itr]=1;
+        for(int i=0;i<v[itr].size();i++)
+            check(v,v[itr][i]);
+
+        return;
+    }
+
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int ans=0; 
+        vector<int> v[isConnected.size()];
+        for(int i=0;i<isConnected.size();i++)
+            for(int j=0;j<isConnected[i].size();j++)
+                if(isConnected[i][j])v[i].push_back(j);
+
+        for(int i=0;i<isConnected.size();i++)
+            if(!visited[i])
+            {
+                check(v,i);
+                ans++;
+            }
+        return ans;
+    }
+};
